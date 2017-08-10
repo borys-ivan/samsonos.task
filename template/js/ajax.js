8aaba2b0e2cb0e0403e92e40ac4292b035a7dfd6
Ivan <borys.ivan@gmail.com>
2017-08-10 14:47:03 +0300

    Add files via upload

---
+++ template/js/ajax.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+
+
+    $("#submit").click(function () {
+
+        var submit = $("#submit").val();
+        var sort = $("#sort").val();
+        var value = $("#value").val();
+        var start_date = $("#start_date").val();
+        var finish_date = $("#finish_date").val();
+        var start_size = $("#start_size").val();
+        var finish_size = $("#finish_size").val();
+
+        $.post('/', {
+            submit: submit, sort: sort, value: value, start_date: start_date, finish_date: finish_date,
+            start_size: start_size, finish_size: finish_size
+        }, function (data) {
+
+            $("#block").html(data);
+        });
+
+        return false;
+    });
+
+
+    $("#default").click(function () {
+
+        var submit = $("#default").val();
+
+
+        $.post('/', {default: submit}, function (data) {
+
+            $("#block").html(data);
+        });
+
+        return false;
+    });
+
+
+    $("a.test_page").click(function () {
+
+        var page = $(this).attr("data-id");
+        $.post('/page-' + page, {}, function (data) {
+
+            $("#block").html(data);
+        });
+
+        return false;
+    });
+
+
+    $("#submit_com").click(function () {
+
+        var submit = $("#submit_com").val();
+        var comment = $("#comment_text").val();
+        var id = $(this).attr("data-id");
+
+        $.post('/update_page/'+id, {submit: submit,comment:comment}, function (data) {
+
+            $("#block_comment").html(data);
+        });
+
+        return false;
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
